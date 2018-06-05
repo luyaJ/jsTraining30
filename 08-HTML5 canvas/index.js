4a595d0b6e5e0f4161a1634ea928d7e7cbc23805
luyaJ <1579618152@qq.com>
2018-06-05 17:23:46 +0800

    canvas画笔

--- 08-HTML5 canvas/index.js
+++ 08-HTML5 canvas/index.js
@@ -5,49 +5,46 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 ctx.strokeStyle = '#bada55';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 50;
-// ctx.globalCompositeOperation = 'multiply';
+ctx.lineJoin = 'round';  // 两线交汇时，创建圆形边角
+ctx.lineCap = 'round';  // 线条末端线帽的样式
+ctx.lineWidth = 10;
 
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+let lastX = 50;  // 起点横坐标
+let lastY = 50;  // 起点纵坐标
 let hue = 0;
 let direction = true;
 
 function draw(e) {
   // stop the fn from running when they are not moused down
   if (!isDrawing) return;
+
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.lineWidth = hue;
+  // ctx.lineWidth = hue;
+  ctx.lineWidth = 20;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY); // start to
-  ctx.lineTo(e.offsetX, e.offsetY);  // go to 
+  ctx.lineTo(e.offsetX, e.offsetY); // go to
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // 使画笔是流畅的
 
   hue++;
   if (hue >= 360) {
     hue = 0;
   }
 
-  if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
-    direction = !direction;
-  }
+  // if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+  //   direction = !direction;
+  // }
 
-  if(direction) {
-    ctx.lineWidth++;
-  } else {
-    ctx.lineWidth--;
-  }
+  // if(direction) {
+  //   ctx.lineWidth++;
+  // } else {
+  //   ctx.lineWidth--;
+  // }
 }
 
-canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
-  // [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-
+canvas.addEventListener('mousedown', e => isDrawing = true);
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
